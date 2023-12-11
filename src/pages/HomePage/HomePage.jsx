d2235b05d124af977cff090138286f9ad59bbfa8
PierreChavez <pchavez@vumigroup.com>
2023-12-11 16:49:48 -0500

    button color

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,16 +3,18 @@ import PageTemplate from '../../components/templates/PageTemplate/PageTemplate';
 import styles from './HomePage.module.css';
 import Card from '../../components/organisms/Card/Card';
 import LoginModal from '../../components/organisms/LoginModal/LoginModal';
+import Button from '../../components/atoms/Button/Button';
 
-const HomePage = ({ onSearchSubmit }) => (
+const HomePage = ({ onSearchSubmit, children }) => (
   <div className={styles.homePage}>
     <PageTemplate onSearchSubmit={onSearchSubmit}>
       <h2>Welcome to our website!</h2>
       <p>This is the home page.</p>
       <Card title="Title">
         <p>Card Content</p>
-      </Card>
+        <Button>Click Me</Button>
 
+      </Card>
     </PageTemplate>
   </div>
 );
